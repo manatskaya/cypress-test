d7399bccfb891e846b3be2f0ba97607c37dad79b
Svitlana <svetlana.manatskaya@gmail.com>
2023-11-09 21:17:54 +0100

    Aqa/cypress locators and actions (#3)
    * Homework Locators and Actions
    
    * added improvements to stepper and form layout
    
    * added additional configurations

--- cypress/e2e/app_tests/cypressInrtro.cy.js
+++ cypress/e2e/app_tests/cypressInrtro.cy.js
@@ -4,3 +4,18 @@ describe('first test suite', () => {
                 cy.get(".form-inline .appearance-filled.size-medium.status-primary.shape-rectangle.transitions").should("contain.text", "Submit")
         });
 });
+describe('second test suite', () => {
+        it('first test',() => {
+                cy.visit('/pages/forms/layouts');
+                cy.get('.inner-circle').eq(0).prev();
+        })
+        it('Verify real event',() => {
+                cy.visit('/pages/forms/layouts');
+                cy.get('span:contains("Option 1")').realClick({pointer:'mouse'});
+                cy.get('#inputEmail1').focus().realType('Some text {enter}');
+        })
+        it('Verify tooltip',() => {
+                cy.visit('pages/modal-overlays/tooltip');
+                cy.contains('Show Tooltip').eq(0).realHover();
+        })
+})
