70e69c4be06a27941ee2a9aeaf8a077dd20a2f81
manatskaya Svitlana <svetlana.manatskaya@gmail.com>
2023-11-12 16:06:25 +0100

    added enhancement to error_validation and form filling using functions

--- cypress/e2e/app_tests/error_validation.cy.js
+++ cypress/e2e/app_tests/error_validation.cy.js
@@ -1,18 +1,17 @@
 describe('Verify fields validation', () => {
     it('Verify header content', () => {
-            cy.visit('/auth/register');
 
-            cy.get('#input-name').focus().realType('N');
-            cy.get("body").click();
+        cy.visit('/auth/register');
+        function verifyHeaderContent(selector, index, errorMessage) {
+            cy.get(selector).focus().realType('N');
+            cy.get('body').click();
             cy.get('.form-control-group .caption.status-danger').as('error');
-            cy.get('@error').eq(0).should('contain', ' Full name should contains from 4 to 50 characters ');
+            cy.get('@error').eq(index).should('contain', errorMessage);
+        }
+            
+        verifyHeaderContent('#input-name', 0, ' Full name should contains from 4 to 50 characters ');
+        verifyHeaderContent('#input-email', 1, ' Email should be the real one! ');
+        verifyHeaderContent('#input-password', 2, ' Password should contain from 4 to 50 characters ');
 
-            cy.get('#input-email').focus().realType('test.com');
-            cy.get("body").click();
-            cy.get('@error').eq(1).should('contain', ' Email should be the real one! ');
-
-            cy.get('#input-password').focus().realType('1');
-            cy.get("body").click();
-            cy.get('@error').eq(2).should('contain', ' Password should contain from 4 to 50 characters ');
     });
 });
--- cypress/e2e/app_tests/form_filling.cy.js
+++ cypress/e2e/app_tests/form_filling.cy.js
@@ -1,17 +1,16 @@
 describe('Verify form filling', () => {
     it('Form filling and data filled', () => {
             cy.visit('/auth/register');
-            cy.get('#input-name').focus().realType('Name');
-            cy.get('input#input-name[ng-reflect-model]').should('have.value', 'Name');
+                function fillAndValidateFormData(selectorWithoutText, inputText, selectorWithText) {
+                    cy.get(selectorWithoutText).focus().realType(inputText);
+                    cy.get(selectorWithText).should('have.value', inputText);
+                }
 
-            cy.get('#input-email').focus().realType('test@email.com')
-            cy.get('input#input-email[ng-reflect-model]').should('have.value', 'test@email.com');
-
-            cy.get('#input-password').focus().realType('123456');
-            cy.get('input#input-password[ng-reflect-model]').should('have.value', '123456');
-
-            cy.get('#input-re-password').focus().realType('123456');
-            cy.get('input#input-re-password[ng-reflect-model]').should('have.value', '123456');
+            fillAndValidateFormData('#input-name', 'Name', 'input#input-name[ng-reflect-model]');
+            fillAndValidateFormData('#input-email', 'test@email.com', 'input#input-email[ng-reflect-model]');
+            fillAndValidateFormData('#input-password', '123456', 'input#input-password[ng-reflect-model]');
+            fillAndValidateFormData('#input-re-password', '123456', 'input#input-re-password[ng-reflect-model]');
+            
 
             cy.get('.label .custom-checkbox').realClick()
                 .should('have.class', 'custom-checkbox checked');
